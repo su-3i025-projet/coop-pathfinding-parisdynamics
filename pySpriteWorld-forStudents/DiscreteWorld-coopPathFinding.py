71bce728f5a2877784043a459d7c222559ca146e
ohouens <rohouens@yahoo.fr>
2019-03-17 18:41:23 +0100

    restructuration du code

--- pySpriteWorld-forStudents/DiscreteWorld-coopPathFinding.py
+++ pySpriteWorld-forStudents/DiscreteWorld-coopPathFinding.py
@@ -15,182 +15,8 @@ import glo
 import random
 import numpy as np
 import sys
-import math
 
-# ---- ---- ---- ---- ---- ----
-# ---- Magie               ----
-# ---- ---- ---- ---- ---- ----
-class PriorityQueue():
-    def __init__(self):
-        self.frontier = []
-
-    def put(self, position, cost):
-        self.frontier.append((position, cost))
-
-    def empty(self):
-        return len(self.frontier) == 0
-
-    def get(self):
-        result = self.frontier[0]
-        for t in self.frontier:
-            if(t[1] != 0 and t[1] < result[1]):
-                result = t
-        pos, cos = result
-        self.frontier.remove(result)
-        return pos, cos
-
-    def clear(self):
-        self.frontier.clear()
-
-class Graph():
-    def __init__(self, world):
-        self.wall, self.goal= world
-
-    def bas(self, position):
-        x, y = position
-        return (x, y-1)
-
-    def droite(self, position):
-        x, y = position
-        return (x+1, y)
-
-    def haut(self, position):
-        x, y = position
-        return (x, y+1)
-
-    def gauche(self, position):
-        x, y = position
-        return (x-1, y)
-
-    def neighbors(self, position):
-        final = []
-        test = [self.haut(position), self.gauche(position), self.droite(position), self.bas(position)]
-        for p in test:
-            if(p not in self.wall and p[0]>=0 and p[0]<=19 and p[1]>=0 and p[1]<=19):
-                final.append(p)
-        return final
-
-    def cost(self, currentP, nextP):
-        xa, ya = currentP
-        xb, yb = nextP
-        return math.sqrt((xb-xa)**2 + (yb-ya)**2)
-
-class Jeu():
-    cpt = 0
-    positions = {}
-    caches = {}
-    caches["l1"] = {}
-    caches["l2"] = []
-
-    def __init__(self, game, player, nom, init, goal, wallStates, goalStates):
-        self.game = game
-        self.player = player
-        self.nom = nom
-        self.position = init
-        self.goal = goal
-        self.graph = Graph((wallStates, goalStates))
-        self.chemin = []
-        self.frontier = PriorityQueue()
-        self.frontier.put(init, 0)
-        self.came_from = {}
-        self.cost_so_far = {}
-        self.came_from[init] = None
-        self.cost_so_far[init] = 0
-        self.pause=0
-        self.ID = Jeu.cpt
-        Jeu.cpt += 1
-        Jeu.positions[self.nom] = init
-        Jeu.caches["l1"][self.nom] = []
-
-    def play(self):
-        i = 1
-        while (not self.frontier.empty()):
-            position, cout = self.frontier.get()
-
-            if(position == self.goal):
-                self.frontier.clear()
-                break
-
-            for nextP in self.graph.neighbors(position):
-                new_cost = self.cost_so_far[position] + self.graph.cost(position, nextP)
-                if nextP not in self.cost_so_far or new_cost < self.cost_so_far[nextP]:
-                    self.cost_so_far[nextP] = new_cost
-                    priority = new_cost + self.heuristic(self.goal, nextP)
-                    self.frontier.put(nextP, priority)
-                    self.came_from[nextP] = position
-            i += 1
-
-        result = self.path()
-        result.reverse()
-        self.chemin.append(result)
-
-    def reset(self):
-        self.frontier.put(self.position, 0)
-        self.came_from = {}
-        self.cost_so_far = {}
-        self.came_from[self.position] = None
-        self.cost_so_far[self.position] = 0
-
-    def heuristic(self,a, b):
-        xa, ya = a
-        xb, yb = b
-        return abs(xa-xb) + abs(ya-yb)
-
-    def path(self):
-        final = []
-        if(self.goal not in self.came_from):
-            return final
-        current = self.goal
-        while(current != None):
-            final.append(current)
-            current = self.came_from[current]
-        return final
-
-    def move(self):
-        Jeu.positions[self.nom] = (-1,-1)
-        # print("chemin: "+str(self.chemin))
-        if(len(self.chemin) == 0):
-            # print("joueur {0}: RIEN A CHERCHER".format(self.nom))
-            Jeu.positions[self.nom] = self.position
-            return
-        if(len(self.chemin[-1]) == 0):
-            # print("joueur {0}: CHEMIN FINI".format(self.nom))
-            Jeu.positions[self.nom] = self.position
-            self.chemin.pop(-1)
-            return
-        if(self.chemin[-1][0] in Jeu.positions.values() and self.pause <= 0):
-            print("joueur {0}: conflit avec un autre joueur".format(self.nom))
-            Jeu.positions[self.nom] = self.position
-            self.pause += 1
-            return
-        elif(self.pause > 1):
-            if(len(Jeu.caches["l1"][self.nom]) > 0):
-                row, col = Jeu.caches["l1"][self.nom][-1]
-            else:
-                return
-            if((row, col) in Jeu.positions.values()):
-                return
-            self.chemin[-1].insert(0, (self.position))
-            self.chemin[-1].insert(0, (-1, -1))
-            Jeu.caches["l1"][self.nom].pop(-1)
-            self.pause -= 1
-        elif(self.pause > 0):
-            print("joueur {0}: passe mon tour".format(self.nom))
-            Jeu.positions[self.nom] = self.position
-            if(self.chemin[-1][0] in Jeu.positions.values()):
-                self.pause += 1
-            else:
-                self.pause -= 1
-            return
-        else:
-            Jeu.caches["l1"][self.nom].append(self.position)
-            row, col = self.chemin[-1][0]
-        self.player.set_rowcol(row, col)
-        self.position = (row, col)
-        self.chemin[-1].pop(0)
-        Jeu.positions[self.nom] = self.position
-        # print("pos :", self.nom, self.position)
-        game.mainiteration()
+from jeu import Jeu
 
 # ---- ---- ---- ---- ---- ----
 # ---- Main                ----
@@ -221,10 +47,6 @@ def main():
 
     init()
 
-
-
-
-
     #-------------------------------
     # Initialisation
     #-------------------------------
@@ -254,15 +76,6 @@ def main():
 
     # on donne a chaque joueur une fiole a ramasser
     # en essayant de faire correspondre les couleurs pour que ce soit plus simple à suivre
-
-
-    #-------------------------------
-    # Boucle principale de déplacements
-    #-------------------------------
-
-
-    # bon ici on fait juste plusieurs random walker pour exemple...
-
     posPlayers = initStates
 
     jeux = []
@@ -275,6 +88,10 @@ def main():
             z=0
         else:
             z+=1
+
+    #-------------------------------
+    # Boucle principale de déplacements
+    #-------------------------------
 
     for i in range(iterations):
         for jeu in jeux:
